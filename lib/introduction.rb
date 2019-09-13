0681c7df7d19cd5fc27bfa0b576d2d31d533d3a6
Solomon Octavius Isaac <octavius.isaac@gmail.com>
2019-09-13 18:47:32 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -7,9 +7,13 @@ def introduction (name)
   puts "Hi, my name is #{name}\n"
 end
 
+introduction ("Octavius")
+
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
 end
+
+introduction_with_language ("Octavius", "Python")
 
 def introduction_with_language_optional (name, language = "Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}\n"
